edebc18c4b2edb660ba3d96adcd6018642edb821
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-11-11 21:28:23 +0000

    optimize

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,3 @@
-import "antd/dist/antd.css";
 import React from "react";
 import { useRouteDate } from "../utils";
 import { Month } from "./Month";
--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,23 @@
 const { createWebpackConfig } = require("@valu/webpack-config");
-module.exports = createWebpackConfig();
+const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
+
+module.exports = createWebpackConfig(
+    {
+        babelPlugins: [
+            [
+                "import",
+                {
+                    libraryName: "antd",
+                    style: "css",
+                },
+            ],
+        ],
+    },
+    config => {
+        config.plugins.push(
+            new MomentLocalesPlugin({
+                localesToKeep: ["fi"],
+            }),
+        );
+    },
+);
